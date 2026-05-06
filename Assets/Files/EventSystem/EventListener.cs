bc0b36bf265d8de5dd42404d8fb13b719626de37
ufatehh0 <ufateh2006@gmail.com>
2026-05-06 14:11:31 +0400

    Event System

---
+++ Assets/Files/EventSystem/EventListener.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EventListener : MonoBehaviour
+{
+    private void OnEnable()
+    {
+      
+        UIButtonTrigger.OnStartPressed += StartTheGame;
+        UIButtonTrigger.OnSettingsPressed += OpenSettings;
+        UIButtonTrigger.OnExitPressed += ExitGame;
+    }
+
+    private void OnDisable()
+    {
+        
+        UIButtonTrigger.OnStartPressed -= StartTheGame;
+        UIButtonTrigger.OnSettingsPressed -= OpenSettings;
+        UIButtonTrigger.OnExitPressed -= ExitGame;
+    }
+
+    void StartTheGame()
+    {
+        Debug.Log("Start Game");
+    }
+
+    void OpenSettings()
+    {
+        Debug.Log("Settings");
+    }
+    void ExitGame()
+    {
+        Debug.Log("Exit Game");
+    }
+}
---
+++ Assets/Files/EventSystem/UIButtonTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System; 
+
+public class UIButtonTrigger : MonoBehaviour
+{
+    
+    public static event Action OnStartPressed;
+    public static event Action OnSettingsPressed;
+    public static event Action OnExitPressed;
+
+    public void TriggerStart()
+    {
+        
+        OnStartPressed?.Invoke();
+    }
+
+    public void TriggerSettings()
+    {
+        OnSettingsPressed?.Invoke();
+    }
+
+
+
+    public void TriggerExit()
+    {
+        OnExitPressed?.Invoke();
+    }
+}
